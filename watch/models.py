6efc09e4bb5598de76f925eeb66c4f779aeb1b31
Sharon Cherop <sh.cherop@gmail.com>
2021-11-01 12:23:38 +0300

    successfully ran first test

--- watch/models.py
+++ watch/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class NeighbourHood(models.Model):
     name=models.CharField(max_length=30)
     location=models.CharField(max_length=30)
+    occupants_count=models.IntegerField()
+    
 
     def create_neighbourhood(self):
         self.save()
-    
+
--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import NeighbourHood
 
 # Create your tests here.
+class NeighbourHoodTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.sharry=NeighbourHood(name='Muthiga Estate',location='Kinoo', occupants_count='null')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sharry,NeighbourHood))
